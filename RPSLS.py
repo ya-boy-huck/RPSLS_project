b6689a28745add7749fb677c3df5171632633d77
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-10-08 14:50:34 -0400

    Initial commit to creat repo and added Samuel as a collaborator

---
+++ RPSLS.py
@@ -0,0 +1,25 @@
+#RPSLS
+
+#GAME RULES
+
+#0. Greeting to the Games
+#1. Display rules (how does the game work?)
+#2. Let user select game mode and number of players
+#3. Display options to the players
+#4. Pick a gesture from list *
+    #5. Second Player/AI picks
+#6. Compare gesture
+    #7. Same gesture - no winner
+    #8. Declare winner of round
+    #9. Give winner a point
+#10. keep playing rounds until we can "declare winner"
+#11. Declare Winner!
+
+
+#Player - Parent class (inherit)
+
+    #Single Player (Human vs AI) - Child
+    #Multiplayer (Human vs Human) - Child
+
+#Gameplay - how the game is played and ran (rules)
+#Main.py
