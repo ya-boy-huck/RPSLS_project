2811023b231885a41dae6d73354ef20d1f824c84
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-10-11 11:31:17 -0400

    Added a validating feeature for user input on line 9 through line 15

--- player.py
+++ player.py
@@ -6,17 +6,29 @@ class Player:
         self.gesture_choice = []
     
     def gesture(self):
-        self.gesture = input("Enter 0 for Rock, 1 for Scissors, 2 for Paper, 3 for Lizard, 4 for Spock")
-        if self.gesture == "0":
-            print(f"You have chosen {self.gesture[0]}")
-        if self.gesture == "1":
-            print(f"You have chosen {self.gesture[1]}")
-        if self.gesture == "2": 
-            print(f"You have chosen {self.gesture[2]}")
-        if self.gesture == "3":        
-            print(f"You have chosen {self.gesture[3]}")
+        while True:
+            self.gesture = input("Enter 0 for Rock, 1 for Scissors, 2 for Paper, 3 for Lizard, 4 for Spock")
+           
+            if self.gesture.lower() not in ('0','1', '2', '3', '4'):
+                     print("Please try again and Enter 0 for Rock, 1 for Scissors, 2 for Paper, 3 for Lizard, 4 for Spock")
+            else:
+                break
 
-    def rules(self)
+            if self.gesture == "0":
+                print(f"You have chosen {self.gesture[0]}")
+            if self.gesture == "1":
+                print(f"You have chosen {self.gesture[1]}")
+            if self.gesture == "2": 
+                print(f"You have chosen {self.gesture[2]}")
+            if self.gesture == "3":        
+                print(f"You have chosen {self.gesture[3]}")
+            if self.gesture == "4":
+                print(f"You have chosen {self.gesture[4]}")
+            else:
+                print("That is not a valid entry, please try again")
+
+    
+    def rules(self):
         for gestures in self.gestures:
            if [0] < [1]:
                player_point += 1
@@ -34,7 +46,4 @@ class Player:
                player_point += 1
            else:
                gesture == gesture
-
-
-    
-
+        pass
