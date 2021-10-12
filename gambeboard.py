5fcc42d79b70b4da423b51a376cc7b21b30ec07b
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-10-12 11:07:03 -0400

    making final touches to gameboard

--- gambeboard.py
+++ gambeboard.py
@@ -26,26 +26,25 @@ class Gameboard:
             print("Rock, Paper, Scissors, Lizard, Spock is the game of rock, paper, scissors woth a twist! Choose wether you would like to play with another person or against the computer. The first player to win 2 out of the 3 rounds will be crowned the winner! Here is some instructions for you to follow. Rock crushes Scissors, Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard, Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard, Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock. Now that has been explained, Enjoy the game!")
         # explain rules
     def game_mode(self):
-        input("Enter 1 to play against another person, or enter 2 to challenge the computer!")
+        input("Enter 1 to play against another person, or enter 2 to challenge the computer!: ")
         if int(input == ()):
             self.player_two = Human()
-        elif int(input == ()):
+        if int(input == ()):
             self.player_two = AI()
-        else:
-            print("please enter the number 1 or 2 to continue")
+             
         # choose game type - Assign a type to Player 2
 
         # Game round phase - Loop
     def player_picks(self):
         # P1 picks
-        self.player_one = Player.choose_gesture(self)
+        self.player_one = Human.choose_gesture(self)
         # P2 picks
         self.player_two = AI.choose_gesture(self)
         
         # Determine winner of round
     def rule_one(self):
         player_point = 0
-        for gesture in self.gesture:
+        for gesture_choice in self.gesture:
             if [0] < [1]:
                 player_point += 1
                 print[f"Player {self.winner_of_round} has won the round"]
@@ -87,7 +86,7 @@ class Gameboard:
                 print[f"Player {self.winner_of_round} has won the round"]
                 return player_point  
             else:
-                gesture == gesture
+                gesture_choice == gesture_choice
                 print("There has been a tie, try again.")
         #Determine the winner of the game
     def rule_two(self):    
